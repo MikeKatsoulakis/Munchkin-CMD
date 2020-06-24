cfc56d54856dbc089764f4d5b9cd6a98cb00ef5b
mike_katsoulakis <mike_katsoulakis@outlook.com>
2020-06-24 23:57:12 +0300

    Added Client to Server Messaging.

--- Client/src/gr/gamedevs/Client/ClientApp.java
+++ Client/src/gr/gamedevs/Client/ClientApp.java
@@ -1,9 +1,78 @@
 package gr.gamedevs.Client;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientApp {
 
+    private static Socket clientSocket;
+    private static PrintWriter out;
+    private static BufferedReader in;
+
+    private static final String IP = "127.0.0.1";
+    private static final int PORT = 4444;
+
+    private static boolean running = true;
+
     public static void main(String[] args) {
-        System.out.println("andriko morio");
+
+        try {
+
+            clientSocket = new Socket(IP, PORT);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            Scanner in = new Scanner(System.in);
+
+            while(running){
+
+                String msg = in.nextLine();
+
+                if(msg.equals("quit")){
+                    stopConnection();
+                    return;
+                }
+
+                sendMessage(msg);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String sendMessage(String msg) {
+
+        try {
+
+            out.println(msg);
+            return in.readLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "ERROR";
+        }
+
+    }
+
+    public static void stopConnection() {
+        try {
+
+            in.close();
+            out.close();
+            clientSocket.close();
+
+            System.out.println("Disconnected..");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- Server/src/gr/gamedevs/Server/ServerApp.java
+++ Server/src/gr/gamedevs/Server/ServerApp.java
@@ -1,8 +1,40 @@
 package gr.gamedevs.Server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerApp {
 
+    private static ServerSocket serverSocket;
+    private static Socket clientSocket;
+    private static PrintWriter out;
+    private static BufferedReader in;
+
+    private static final int PORT = 4444;
+
     public static void main(String[] args) {
+
+        try {
+
+            serverSocket = new ServerSocket(PORT);
+            clientSocket = serverSocket.accept();
+            System.out.println("Connected..");
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            while(clientSocket.isConnected()){
+                String msg = in.readLine();
+                System.out.println(msg);
+                //out.println(msg);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
