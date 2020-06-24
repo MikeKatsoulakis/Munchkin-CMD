d8b821767f088e226ca8f8f09eadedd81828ac05
mike_katsoulakis <mike_katsoulakis@outlook.com>
2020-06-24 17:02:12 +0300

    Basic files

---
+++ Client/src/gr/gamedevs/Client/ClientApp.java
@@ -0,0 +1,9 @@
+package gr.gamedevs.Client;
+
+public class ClientApp {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Server/src/gr/gamedevs/Server/ServerApp.java
@@ -0,0 +1,9 @@
+package gr.gamedevs.Server;
+
+public class ServerApp {
+
+    public static void main(String[] args) {
+
+    }
+
+}
